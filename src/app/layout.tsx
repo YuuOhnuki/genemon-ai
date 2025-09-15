77287d5a09da3dd2417e8fb3d200ded084ebb1bd
YuuOhnuki <its.yuu.ohnuki@gmail.com>
2025-09-15 21:04:40 +0900

    Vercel AnalyticsとSpeed Insightsの追加
    📦deps: package.jsonに@vercel/analyticsと@vercel/speed-insightsを追加
    📦deps: package-lock.jsonを更新
    💄style: layout.tsxにSpeedInsightsとAnalyticsコンポーネントを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
+
 const geistSans = Geist({
     variable: '--font-geist-sans',
     subsets: ['latin'],
@@ -23,7 +26,9 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="ja">
+            <SpeedInsights />
+            <Analytics />
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
         </html>
     );
